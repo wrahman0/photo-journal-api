e64989e5c60ee99430eb93d538043658e3f8207d
Wasiur Rahman <wrahman0@gmail.com>
2015-04-05 22:29:42 -0400

    User exists error

--- app/common/errors.js
+++ app/common/errors.js
@@ -3,12 +3,12 @@
 var util = require("util");
 var _ = require("lodash");
 
-function NoDefaultAppId(appKeyName) {
+function UserExists(appKeyName) {
     this.message = "Cannot resolve default AppId for AppKey: " + appKeyName;
 }
-util.inherits(NoDefaultAppId, Error);
+util.inherits(UserExists, Error);
 
 module.exports = {
-    NoDefaultAppIdError: NoDefaultAppId
+    UserExistsError: UserExists
 };
 
