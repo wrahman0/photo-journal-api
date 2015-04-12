c3d1e8b1ea92efdee6075eab0acc6f20292975c0
Wasiur Rahman <wrahman0@gmail.com>
2015-04-12 13:37:23 -0400

    Added more tests

--- test/authentication/authenticationHelpers.test.js
+++ test/authentication/authenticationHelpers.test.js
@@ -8,7 +8,10 @@ var bcrypt = require('bcryptjs');
 describe.only('Authentication Helpers', function () {
 
     var testVariables = {
-        password: 'password'
+        password: 'password',
+        payload: {
+            key: "value"
+        }
     };
 
     before(function () {
@@ -33,6 +36,21 @@ describe.only('Authentication Helpers', function () {
 
         it ('should return false when the plaintext password doesnt match hash', function(done){
             expect(authenticationHelpers.isValidPassword(testVariables.password, '')).to.equal(false);
+            done();
+        });
+    });
+
+    describe('Payload encoding and decoding', function () {
+        it ('should encode the payload into a token when a valid payload is sent', function(done){
+            var token = authenticationHelpers.encodePayload(testVariables.payload);
+            expect(token).to.not.equal(testVariables.payload);
+            done();
+        });
+
+        it ('should decode to payload when a valid token corresponding to the payload is sent', function(done){
+            var token = authenticationHelpers.encodePayload(testVariables.payload);
+            var payload = authenticationHelpers.decodePayload(token);
+            expect(_.isEqual(payload, testVariables.payload)).to.equal(true);
             done();
         });
     });
