a7c5554081de4f8a66024839d41a96da13f011f2
Wasiur Rahman <wrahman0@gmail.com>
2015-04-15 22:27:24 -0400

    Removed comments

--- app/common/validateParams.js
+++ app/common/validateParams.js
@@ -4,29 +4,6 @@ var Promise = require('bluebird');
 var _ = require('lodash');
 var errors = require('./errors');
 
-/**
- * Validates a number of parameters for a request handler. Can handle params in the request route, query or body (or
- * anything else), and also handles asynchronous validators.
- *
- * All validators should be paired with an error type to include in the ValidationError. (see errors.js)
- * All parameter validation descriptions should have at least one of required: true or a validator/error pair. Both are
- * allowed on the same property.
- *
- * Usage:
- * validateParams([
- *     // Ensure that req.params.testParam exists, otherwise throws a MissingArgumentError
- *     {name: 'testParam', in: req.params, required: true},
- *     // Ensure that if the version exists, the version is valid semver, otherwise throw an InvalidSemverFormatError
- *     {name: 'version', in: req.body, validator: semver.valid, error: InvalidSemverFormatError},
- * ]).then(function() {
- *     // All validations passed! Handle the request and response
- * }).catch(ValidationError, function(err) {
- *     // Handle the validation errors. Probably want to respond with a 400
- * });
- *
- * @param params The list of parameters to validate
- * @returns {Promise<R[]>} A promise of a list of asynchronous validation checks. Will complete when all validations have.
- */
 module.exports = function (params) {
     var exists = function (value) {
         return !(_.isUndefined(value) || _.isNull(value) || _.isEmpty(value));
